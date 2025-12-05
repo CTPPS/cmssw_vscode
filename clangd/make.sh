e6ff370b0d8863128a2bf543862f4e845eaefe8f
Pietro Grutta <pietrogrutta@gmail.com>
2025-12-05 20:26:38 +0100

    Addressed comments in the PR

--- clangd/make.sh
+++ clangd/make.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 scram b llvm-ccdb -j 4
+if [ -z "$CMSSW_BASE" ]; then
+    # echo "Warning: unset cmsenv"
+    cmsenv
+fi
 python3 $CMSSW_BASE/make_compile_commands.py
-#scram b -j 16
--- clangd/make_compile_commands.py
+++ clangd/make_compile_commands.py
@@ -1,41 +1,38 @@
+from pathlib import Path
 import json
 import os
 
-if not all(item in os.environ for item in ['CMSSW_BASE', 'CMSSW_RELEASE_BASE']):
+if not all([item in os.environ for item in ["CMSSW_BASE", "CMSSW_RELEASE_BASE"]]):
     raise Exception("Run cmsenv first")
 
-def appendSlash(string):
-    if len(string) > 0 and string[-1]!='/':
-        string = string + '/'
-    return string
+CMSSW_BASE = Path(os.environ["CMSSW_BASE"])
+CMSSW_RELEASE_BASE = Path(os.environ["CMSSW_RELEASE_BASE"])
 
-
-# Append the '/' at the end of paths, if not present
-CMSSW_BASE = appendSlash(os.getenv('CMSSW_BASE'))
-CMSSW_RELEASE_BASE = appendSlash(os.getenv('CMSSW_RELEASE_BASE'))
-
-
-# Load the JSON files.
-with open(CMSSW_BASE+"compile_commands.json", "r") as f:
+# Load the local/release compilation commands
+with (CMSSW_BASE / "compile_commands.json").open("r") as f:
     compile_commands_preFix = json.load(f)
-with open(CMSSW_RELEASE_BASE+"compile_commands.json", "r") as f:
+with (CMSSW_RELEASE_BASE / "compile_commands.json").open("r") as f:
     compile_commands_release = json.load(f)
-    files_release = [item['file'] for item in compile_commands_release]
+    files_release_map = {
+        item["file"]: idx for idx, item in enumerate(compile_commands_release)
+    }
 
-
-# Update the compile commands of the release with those of the local base
+# Update the compile commands of the release with the local area changes
 for entry in compile_commands_preFix:
-    file = entry['file']
-    if file in files_release:
-        pos = files_release.index(file)
-        compile_commands_release[pos]['directory'] = entry['directory']
-        compile_commands_release[pos]['command'] = entry['command']
+    file = entry["file"]
+    if file in files_release_map:
+        pos = files_release_map[file]
+        compile_commands_release[pos]["directory"] = entry["directory"]
+        compile_commands_release[pos]["command"] = entry["command"]
     else:
         compile_commands_release.append(
-            {'command': entry['command'], 'file': entry['file'], 'directory': entry['directory']}
-            )
+            {
+                "command": entry["command"],
+                "file": entry["file"],
+                "directory": entry["directory"],
+            }
+        )
 
 # Store the new commands into the compile_commands.json
-with open(CMSSW_BASE+"compile_commands.json", "w") as f:
+with (CMSSW_BASE / "compile_commands.json").open("w") as f:
     json.dump(compile_commands_release, f, indent=4)
-
